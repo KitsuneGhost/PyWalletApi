8f539484f03515542f519b0a3dd855107954da59
KitsuneGhost <3722146@gmail.com>
2026-01-25 14:53:01 +0100

    added roles_required decorator to to implement role checks

--- app/auth/decorators.py
+++ app/auth/decorators.py
@@ -35,31 +35,65 @@ class Decorator:
         return token
 
     def jwt_required(self):
+        """
+        Verifies user jwt and sets up current user and payload.
+        """
+
         def decorator(fn):
             @wraps(fn)
             def wrapper(*args, **kwargs):
+
                 try:
+                    # loading token and user data
                     token = self._get_bearer_token()
                     payload = self.token_provider.verify(token, expected_type="access")
                     user_id = int(payload["sub"])
                     user = self.user_repo.get_by_id(user_id)
+
+                    # checking if user exists
                     if not user:
                         return jsonify({"error": "User doesnt exist"}), 401
+
+                    # setting up current user and payload
                     g.current_user = user
                     g.token_payload = payload
+
                     return fn(*args, **kwargs)
+
                 except UnauthorizedAttempt:
                     return jsonify({"error": "Unauthorized"}), 401
+
                 except TokenExpired:
                     return jsonify({"error": "Token is expired"}), 401
+
                 except TokenInvalid:
                     return jsonify({"error": "Token is invalid"}), 401
+
                 except ValueError:
                     return jsonify({"error": "Invalid subject claim"}), 401
+
                 except Exception as e:
                     return jsonify({"error": "Internal server error"}), 500
 
             return wrapper
-
         return decorator
 
+    def roles_required(self, *roles: str):
+        """
+        Checks if current user has required role. Should run after jwt_required()
+        """
+
+        def decorator(fn):
+            @wraps(fn)
+            def wrapper(*args, **kwargs):
+
+                user = getattr(g, "current_user", None)
+                if not user:
+                    return jsonify({"error": "Unauthorized"}), 401
+
+                if user.role not in roles:
+                    return jsonify({"error": "Forbidden"}), 403
+
+                return fn(*args, **kwargs)
+            return wrapper
+        return decorator
